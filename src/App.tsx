3d49931067bf6ef17a87259a99e409c4a19d987f
ictus <ictus@ictusui-MacBookPro.local>
2022-05-18 10:33:26 +0900

    Radio button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MuiTypography from "./components/MuiTypography";
 import MuiButton from "./components/MuiButton";
 import MuiTextField from "./components/MuiTextField";
 import MuiSelect from "./components/MuiSelect";
+import MuiRadioButton from "./components/MuiRadioButton";
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             {/*<MuiTypography/>*/}
             {/*<MuiButton/>*/}
             {/*<MuiTextField/>*/}
-            <MuiSelect/>
+            {/*<MuiSelect/>*/}
+            <MuiRadioButton/>
         </div>
     );
 }
